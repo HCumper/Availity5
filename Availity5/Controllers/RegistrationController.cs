4daf62c6395473e56ae5664fdfbdbaa6903969f4
Hugh Cumper <hcumper@usa.net>
2020-05-23 14:14:44 -0400

    Add project files.

---
+++ Availity5/Controllers/RegistrationController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Availity5.DataModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Availity5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    class RegistrationController
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<RegistrationController> _logger;
+
+        public RegistrationController(ILogger<RegistrationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Registration> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Registration
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+    }
+}
