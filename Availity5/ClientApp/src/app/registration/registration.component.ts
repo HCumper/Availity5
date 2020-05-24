6d8bd8b1dd54ad7840a2a8d9536a6b9e5a3f5cf0
Hugh Cumper <hcumper@usa.net>
2020-05-24 19:31:38 -0400

    move email validation

--- Availity5/ClientApp/src/app/registration/registration.component.ts
+++ Availity5/ClientApp/src/app/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn, FormArray } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 import { Registration } from './registration';
 
 @Component({
@@ -11,6 +12,12 @@ import { Registration } from './registration';
 export class RegistrationComponent implements OnInit {
   registrationForm: FormGroup;
   registration = new Registration();
+  emailMessage: string;
+
+  private ValidationMessages = {
+    required: 'Please enter the email address.',
+    email: 'Invalid email address'
+}
 
   constructor(private fb: FormBuilder) { }
 
@@ -22,9 +29,16 @@ export class RegistrationComponent implements OnInit {
       TelephoneNumber: ['', [Validators.required, Validators.pattern("[0-9]{10}")]],
 
     })
+
+    const emailControl = this.registrationForm.controls.Email;
+    emailControl.valueChanges.pipe(
+      debounceTime(1000)
+    ).subscribe(
+      value => this.setMessage(emailControl)
+    );
   }
 
-  setEmailOrOPhone(): void {
+  setEmailOrPhone(): void {
     const phoneControl = this.registrationForm.get('TelephoneNumber');
     const emailControl = this.registrationForm.get('Email');
 
@@ -37,4 +51,11 @@ export class RegistrationComponent implements OnInit {
     console.log('Saved: ' + JSON.stringify(this.registrationForm.value));
   }
 
+  setMessage(c: AbstractControl): void {
+    this.emailMessage = '';
+    if ((c.touched || c.dirty) && c.errors) {
+      this.emailMessage = Object.keys(c.errors).map(
+        key => this.ValidationMessages[key]).join(' ');
+    }
+  }
 }
